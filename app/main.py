0cd7bd39fa474e678946bad07cf8048de4a8fac0
khalidrazaa <khalid_raza@live.com>
2026-02-01 02:19:40 +0530

    added mongodb as scrap and save db

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from app.api.router import router as api_router
 from app.db.session import engine
 import asyncio
 import os
+from app.db.mongodb import get_mongo_db
 
 app = FastAPI()
 
@@ -33,10 +34,19 @@ async def on_startup():
     except Exception as e:
         print(f"❌ Database connection failed: {e}")
 
+        # MongoDB check
+    try:
+        mongo_db = get_mongo_db()
+        # Perform a simple command to ensure connection
+        mongo_db.command("ping")
+        print("✅ MongoDB connection successful.")
+    except Exception as e:
+        print(f"❌ MongoDB connection failed: {e}")
+
 
 @app.get("/")
 async def root():
-    return {"message": "ExplainIt.Tech up and Running"}
+    return {"message": "fastapi backend app  up and Running"}
 
 
 if __name__ == "__main__":
