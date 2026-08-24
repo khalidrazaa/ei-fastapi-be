6b8dec49141120028d736a61033aca7db5516048
Khalid Raza <khalid_raza@live.com>
2026-08-24 22:07:39 +0530

    Update yt_video_query.py

--- app/db/query/yt_video_query.py
+++ app/db/query/yt_video_query.py
@@ -80,6 +80,8 @@ async def query_by_niche(
         .order_by(*order_by_clause)
     )
 
+    print(final_query)
+
     # 5. Pagination + total + metadata
     return await paginate_query(
         db=db,
